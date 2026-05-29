6ec417fadeecd8cef717609c989ec3e92e7e5630
y-yasuda <y-yasuda@CD26311L.arksystems.co.jp>
2026-05-29 14:24:06 +0900

    Practice5-06まで完了

--- spring_practice/src/main/java/jp/co/sss/practice/p05/q05/controller/Practice0505Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/q05/controller/Practice0505Controller.java
@@ -1,5 +1,38 @@
 package jp.co.sss.practice.p05.q05.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.sss.practice.p05.bean.FruitsSeasonBean;
+import jp.co.sss.practice.p05.entity.FruitsSeason;
+import jp.co.sss.practice.p05.form.FruitsSeasonForm;
+import jp.co.sss.practice.p05.repository.FruitsSeasonRepository;
+
+@Controller
 public class Practice0505Controller {
+	@Autowired
+	FruitsSeasonRepository repository;
+
+	@RequestMapping(path = "/fruits/add/input", method = RequestMethod.GET)
+	public String input() {
+		return "practice05/05/fruit_input";
+	}
+
+	@RequestMapping(path = "/fruits/add/cpmplete", method = RequestMethod.POST)
+	public String complete(FruitsSeasonForm fruitForm, Model model) {
+		FruitsSeason fruitsSeason = new FruitsSeason();
+		BeanUtils.copyProperties(fruitForm, fruitsSeason, "fruitId");
+		fruitsSeason = repository.save(fruitsSeason);
+
+		FruitsSeasonBean fruitsSeasonBean = new FruitsSeasonBean();
+		BeanUtils.copyProperties(fruitsSeason, fruitsSeasonBean);
+		model.addAttribute("fruitSeason", fruitsSeasonBean);
+
+		return "practice05/05/fruit_detail";
+	}
 
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p05/q06/controller/Practice0506Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/q06/controller/Practice0506Controller.java
@@ -1,5 +1,52 @@
 package jp.co.sss.practice.p05.q06.controller;
 
-public class Practice0506Controller {
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.practice.p05.bean.FruitsSeasonBean;
+import jp.co.sss.practice.p05.entity.FruitsSeason;
+import jp.co.sss.practice.p05.form.FruitsSeasonForm;
+import jp.co.sss.practice.p05.repository.FruitsSeasonRepository;
+
+@Controller
+public class Practice0506Controller {
+	@Autowired
+	FruitsSeasonRepository repository;
+
+	@RequestMapping(path = "/fruits/update", method = RequestMethod.GET)
+	public String update(Model model) {
+		model.addAttribute("fruits", repository.findAll());
+
+		return "practice05/06/fruit_select";
+	}
+
+	@RequestMapping(path = "/fruits/update/input", method = RequestMethod.POST)
+	public String input(Integer fruitId, Model model) {
+		FruitsSeason fruitsSeason = repository.getReferenceById(fruitId);
+		FruitsSeasonBean fruitsSeasonBean = new FruitsSeasonBean();
+		BeanUtils.copyProperties(fruitsSeason, fruitsSeasonBean);
+
+		model.addAttribute("fruit", fruitsSeasonBean);
+		return "practice05/06/fruit_input";
+	}
+
+	@RequestMapping(path = "/fruits/update/complete/{fruitId}", method = RequestMethod.POST)
+	public String complete(@PathVariable Integer fruitId, FruitsSeasonForm fruitForm, Model model) {
+		FruitsSeason fruitsSeason = new FruitsSeason();
+		BeanUtils.copyProperties(fruitForm, fruitsSeason);
+		fruitsSeason = repository.save(fruitsSeason);
+
+		FruitsSeasonBean fruitsSeasonBean = new FruitsSeasonBean();
+		BeanUtils.copyProperties(fruitsSeason, fruitsSeasonBean);
+
+		model.addAttribute("fruit", fruitsSeasonBean);
+
+		return "practice05/06/fruit_detail";
+
+	}
 }
